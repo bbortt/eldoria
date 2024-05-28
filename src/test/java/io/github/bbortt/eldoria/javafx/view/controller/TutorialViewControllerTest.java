f10aeaea4c065f3112e2bea2f64c0621a6b2429f
Timon Borter <timon.borter@gmx.ch>
2024-05-28 16:24:28 +0200

    ci: fix tutorial view controller test

--- src/test/java/io/github/bbortt/eldoria/javafx/view/controller/TutorialViewControllerTest.java
+++ src/test/java/io/github/bbortt/eldoria/javafx/view/controller/TutorialViewControllerTest.java
@@ -1,12 +1,19 @@
 package io.github.bbortt.eldoria.javafx.view.controller;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.util.ReflectionTestUtils.setField;
 
+import io.github.bbortt.eldoria.domain.UserPreferences;
 import io.github.bbortt.eldoria.service.GameService;
 import io.github.bbortt.eldoria.service.UserPreferencesService;
+import java.util.Locale;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,24 +37,55 @@ class TutorialViewControllerTest {
     @Mock
     private UserPreferencesService userPreferencesServiceMock;
 
+    @Mock
+    private VBox viewBoxMock;
+
+    @Mock
+    public Label tutorialTextMock;
+
+    @Mock
+    public VBox actionContainerMock;
+
+    @Mock
+    private ObservableList<Node> actionContainerChildrenMock;
+
     private TutorialViewController fixture;
 
     @BeforeEach
     void setUp() {
         fixture = new TutorialViewController(messageSourceMock, gameServiceMock, userPreferencesServiceMock);
+
+        setField(fixture, "viewBox", viewBoxMock, VBox.class);
+        setField(fixture, "tutorialText", tutorialTextMock, Label.class);
+        setField(fixture, "actionContainer", actionContainerMock, VBox.class);
     }
 
     @Nested
     class Initialize {
 
+        @BeforeEach
+        void setUp() {
+            doReturn(actionContainerChildrenMock).when(actionContainerMock).getChildren();
+        }
+
         @Test
         void configuresBackground() {
-            var viewBoxMock = mock(VBox.class);
-            setField(fixture, "viewBox", viewBoxMock, VBox.class);
+            var userPreferences = new UserPreferences();
+            doReturn(userPreferences).when(userPreferencesServiceMock).loadUserPreferences();
+
+            doReturn("Welcome, welcome!")
+                .when(messageSourceMock)
+                .getMessage(
+                    argThat(string -> string.equals("tutorial.welcome.introduction") || string.equals("global.button.continue")),
+                    isNull(),
+                    any(Locale.class)
+                );
 
             fixture.initialize();
 
             verify(viewBoxMock).setBackground(any(Background.class));
+            verify(tutorialTextMock).setText("");
+            verify(actionContainerChildrenMock).clear();
         }
     }
 }
