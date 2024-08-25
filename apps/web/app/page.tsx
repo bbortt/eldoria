63d1e529fe114218e3e76b8031592ee1202a8790
Timon Borter <timon.borter@gmx.ch>
2024-08-25 21:04:49 +0200

    chore: prepare tutorial layout

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,16 +2,19 @@ import styles from './page.module.css';
 
 import MainMenu from '@/layout/main-menu';
 
-export default function Home() {
+export const Home = () => {
   return (
-    <div className={styles.page}>
-      <h1 className={styles.title}>
-        Chronicles of Eldoria <small>The Shadowed Realm</small>
-      </h1>
-
-      <MainMenu />
-
-      <footer className={styles.footer}></footer>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>
+          Chronicles of Eldoria <small>The Shadowed Realm</small>
+        </h1>
+      </header>
+      <main className={styles.main}>
+        <MainMenu />
+      </main>
     </div>
   );
-}
+};
+
+export default Home;
--- apps/web/src/layout/main-menu.tsx
+++ apps/web/src/layout/main-menu.tsx
@@ -1,11 +1,11 @@
 'use client';
 
+import Link from 'next/link';
+
 import { useState } from 'react';
 
 import { DefaultButton } from '@repo/ui/components';
 import { AnimatePresence, motion } from '@repo/ui/lib';
-
-import styles from './main-menu.module.css';
 
 const buttonVariants = {
   initial: { opacity: 0, y: 50 },
@@ -17,58 +17,58 @@ export const MainMenu = () => {
   const [startGame, setStartGame] = useState(false);
 
   return (
-    <main className={styles.main}>
-      <AnimatePresence mode="wait">
-        {startGame ? (
-          <motion.div
-            key="start-game-options"
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={{
-              initial: { opacity: 0 },
-              animate: { opacity: 1 },
-              exit: { opacity: 0 },
-            }}
-            transition={{ duration: 0.3 }}
-            className="flex flex-col gap-y-2 items-center"
-          >
-            <motion.div variants={buttonVariants} transition={{ delay: 0.1 }}>
+    <AnimatePresence mode="wait">
+      {startGame ? (
+        <motion.div
+          key="start-game-options"
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          variants={{
+            initial: { opacity: 0 },
+            animate: { opacity: 1 },
+            exit: { opacity: 0 },
+          }}
+          transition={{ duration: 0.3 }}
+          className="flex flex-col gap-y-2 items-center"
+        >
+          <motion.div variants={buttonVariants} transition={{ delay: 0.1 }}>
+            <Link href="/tutorial/1">
               <DefaultButton>Start Tutorial</DefaultButton>
-            </motion.div>
-            <motion.div variants={buttonVariants} transition={{ delay: 0.2 }}>
-              <DefaultButton>Configure Game</DefaultButton>
-            </motion.div>
-            <motion.div variants={buttonVariants} transition={{ delay: 0.3 }}>
-              <DefaultButton color="warning" onClick={() => setStartGame(false)}>
-                Cancel
-              </DefaultButton>
-            </motion.div>
+            </Link>
           </motion.div>
-        ) : (
-          <motion.div
-            key="main-options"
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={{
-              initial: { opacity: 0 },
-              animate: { opacity: 1 },
-              exit: { opacity: 0 },
-            }}
-            transition={{ duration: 0.3 }}
-            className="flex flex-col gap-y-2 items-center"
-          >
-            <motion.div variants={buttonVariants} transition={{ delay: 0.1 }}>
-              <DefaultButton onClick={() => setStartGame(true)}>Start Game</DefaultButton>
-            </motion.div>
-            <motion.div variants={buttonVariants} transition={{ delay: 0.2 }}>
-              <DefaultButton>Load Game</DefaultButton>
-            </motion.div>
+          <motion.div variants={buttonVariants} transition={{ delay: 0.2 }}>
+            <DefaultButton>Configure Game</DefaultButton>
           </motion.div>
-        )}
-      </AnimatePresence>
-    </main>
+          <motion.div variants={buttonVariants} transition={{ delay: 0.3 }}>
+            <DefaultButton color="warning" onClick={() => setStartGame(false)}>
+              Cancel
+            </DefaultButton>
+          </motion.div>
+        </motion.div>
+      ) : (
+        <motion.div
+          key="main-options"
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          variants={{
+            initial: { opacity: 0 },
+            animate: { opacity: 1 },
+            exit: { opacity: 0 },
+          }}
+          transition={{ duration: 0.3 }}
+          className="flex flex-col gap-y-2 items-center"
+        >
+          <motion.div variants={buttonVariants} transition={{ delay: 0.1 }}>
+            <DefaultButton onClick={() => setStartGame(true)}>Start Game</DefaultButton>
+          </motion.div>
+          <motion.div variants={buttonVariants} transition={{ delay: 0.2 }}>
+            <DefaultButton>Load Game</DefaultButton>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
