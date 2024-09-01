07154122c9d012ad7dd3a82c17fa225f713ac027
Timon Borter <timon.borter@gmx.ch>
2024-09-01 19:22:48 +0200

    fix(github-pages): release with base path

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath:process.env.ELDORIA_BASE_PATH ?? '',
   output: 'export',
   reactStrictMode: true,
 	transpilePackages: ["ui", "tailwind-config"],
