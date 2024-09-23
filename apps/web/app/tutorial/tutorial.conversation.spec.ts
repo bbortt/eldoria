1459a9c29cc60b229366726a5ab7ef27f3e87b7d
Timon Borter <timon.borter@gmx.ch>
2024-09-23 19:20:34 +0200

    ci: add base path test

--- apps/web/app/tutorial/tutorial.conversation.spec.ts
+++ apps/web/app/tutorial/tutorial.conversation.spec.ts
@@ -1,8 +1,6 @@
 import { existsSync } from 'node:fs';
 import { join } from 'node:path';
 
-import tutorialConversation from './tutorial.conversation';
-
 describe('Tutorial', () => {
   const publicDir = join(process.cwd(), 'public');
 
@@ -11,7 +9,15 @@ describe('Tutorial', () => {
   };
 
   describe('Conversation', () => {
+    let originalEnv;
+
+    beforeEach(() => {
+      originalEnv = process.env;
+    });
+
     test('all background images exist in public folder', () => {
+      const tutorialConversation = require('./tutorial.conversation');
+
       Object.values(tutorialConversation).forEach(conversation => {
         if (conversation.backgroundImage) {
           const imagePath = join(publicDir, conversation.backgroundImage);
@@ -21,6 +27,23 @@ describe('Tutorial', () => {
           }
         }
       });
+    });
+
+    test('all background images have the correct basePath prefix', () => {
+      const mockBasePath = '/mock-base-path';
+      process.env.NEXT_PUBLIC_ELDORIA_BASE_PATH = mockBasePath;
+
+      const tutorialConversation = require('./tutorial.conversation');
+
+      Object.values(tutorialConversation).forEach(conversation => {
+        if (conversation.backgroundImage) {
+          expect(conversation.backgroundImage.startsWith(mockBasePath)).toBeTruthy();
+        }
+      });
+    });
+
+    beforeEach(() => {
+      process.env = originalEnv;
     });
   });
 });
