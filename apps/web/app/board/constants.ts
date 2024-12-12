3177ae670ef3563e3f7d41bc1431e878de32ebae
Timon Borter <timon.borter@gmx.ch>
2024-12-12 09:02:47 +0100

    chore(grid): more sofisticated colors

--- apps/web/app/board/constants.ts
+++ apps/web/app/board/constants.ts
@@ -1,1 +1,5 @@
 export const DRAGGABLE_TYPE_CHARACTER = 'draggable:character';
+
+export const CELL_BACKGROUND_VALID = 'bg-success/50';
+export const CELL_BACKGROUND_BRIGHT = 'bg-secondary/30';
+export const CELL_BACKGROUND_DARK = 'bg-secondary/10';
