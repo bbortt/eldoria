49d2abbfd2fcecd3efe7b9db01294a10d9ecdb5a
Timon Borter <timon.borter@gmx.ch>
2025-01-18 12:30:31 +0100

    ci: add test reports

--- apps/web/.jest.config.ts
+++ apps/web/.jest.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
 
+const { CI } = process.env;
+
 const createJestConfig = nextJest({
   dir: './',
 });
@@ -12,6 +14,12 @@ const config: Config = {
     '^@/(.*)$': '<rootDir>/src/$1',
     'lucide-react': require.resolve('lucide-react'),
   },
+  reporters: [
+    // @ts-expect-error - type incompatibility
+    ...(CI ? [['github-actions', { silent: false }], 'summary'] : ['default']),
+    // @ts-expect-error - type incompatibility
+    ['jest-junit', { outputDirectory: 'reports', outputName: 'report.xml' }],
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest.setup.ts'],
   testEnvironment: 'jsdom',
 };
